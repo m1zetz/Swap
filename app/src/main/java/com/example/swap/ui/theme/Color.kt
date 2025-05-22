2e27db8fac7019c96df63052fd5279b80f4fe960
m1zetz <riywer80@gmail.com>
2025-05-23 01:39:39 +0500

    thirdCommit

--- app/src/main/java/com/example/swap/ui/theme/Color.kt
+++ app/src/main/java/com/example/swap/ui/theme/Color.kt
@@ -10,7 +10,8 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val backgroundColorPurple = Color(0xFF9500FF)
+val backgroundColorPurple1 = Color(0xFF7E00D9)
+val backgroundColorPurple2 = Color(0xFFD07EFF)
 val whiteForText = Color.White
 val whiteOrange = Color(0xFFffe1db)
-val whiteRed = Color(0xFFffd3d3)
+val whiteRed = Color(0xFFFFBFBF)
